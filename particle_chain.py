13801ad97cc6a169087d44f2573fb172d0b9d4b1
Toms Bauģis <toms.baugis@gmail.com>
2010-01-07 14:29:53 +0000

    added color

--- particle_chain.py
+++ particle_chain.py
@@ -8,15 +8,20 @@ from hamster import pytweener
 from hamster import graphics
 from hamster.pytweener import Easing
 
+import colorsys
+
 class TailParticle(object):
-    def __init__(self, x, y, follow = None):
+    def __init__(self, x, y, color, follow = None):
         self.x = x
         self.y = y
         self.follow = follow
+        self.color = color
     
     def draw(self, area):
         area.draw_rect(self.x - 5, self.y - 5, 10, 10, 3)
-        area.set_color("#121266")
+        
+        
+        area.set_color(self.color)
         area.context.fill()
         if self.follow:
             area.context.move_to(self.x, self.y)
@@ -28,11 +33,12 @@ class Canvas(graphics.Area):
         graphics.Area.__init__(self)
 
         self.tail = []
-        for i in range(50):
+        parts = 30
+        for i in range(parts):
             previous = self.tail[-1] if self.tail else None
-
+            color = colorsys.hls_to_rgb(0.6, i / float(parts), 1)
             
-            self.tail.append(TailParticle(10, 10, previous))
+            self.tail.append(TailParticle(10, 10, color, previous))
 
         self.connect("motion_notify_event", self.on_mouse_move)        
         self.mouse_moving = False
@@ -55,14 +61,14 @@ class Canvas(graphics.Area):
             self.tweener.killTweensOf(particle)
             
             
-            self.tweener.addTween(particle, **dict(x=new_x, y=new_y, tweenType = Easing.Expo.easeOut, tweenTime=0.5))
+            self.tweener.addTween(particle, **dict(x=new_x, y=new_y, tweenType = Easing.Elastic.easeOut, tweenTime=0.5))
         
         self.mouse_moving = True
 
 
     def on_expose(self):
         # on expose is called when we are ready to draw
-        for tail in self.tail:
+        for tail in reversed(self.tail):
             tail.draw(self)
 
         if self.mouse_moving == False:
@@ -70,10 +76,8 @@ class Canvas(graphics.Area):
             for particle in reversed(self.tail):
                 if particle.follow:
                     new_x, new_y = particle.follow.x, particle.follow.y
-                    
                     [self.tweener.removeTween(tween) for tween in self.tweener.getTweensAffectingObject(particle)]
-                    
-                    self.tweener.addTween(particle, **dict(x=new_x, y=new_y, tweenType = Easing.Expo.easeOut, tweenTime=0.5))
+                    self.tweener.addTween(particle, **dict(x=new_x, y=new_y, tweenType = Easing.Elastic.easeOut, tweenTime=0.5))
 
         self.mouse_moving = False    
         self.redraw_canvas() # constant redraw (maintaining the requested frame rate)
@@ -100,21 +104,3 @@ if __name__ == "__main__":
     example = BasicWindow()
     gtk.main()
 
-    """
-    some profiling rubbish
-    tweener = pytweener.Tweener()
-    
-    particles = []
-    
-    # create
-    for i in range(2000):
-        tp = TailParticle(10, 10)
-        particles.append(tp)
-        tweener.addTween(tp, x=10, y=15)
-    
-    # stop and update
-    for particle in particles:
-        tweens = tweener.killTweensOf(particle)
-        tweener.addTween(particle, x=20, y=30)
-
-    """
