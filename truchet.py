6f1eef405412d4bfd10334886277c2a9dc72d113
Toms Bauģis <toms.baugis@gmail.com>
2010-01-26 04:19:07 +0000

    some cleanup and added line fill which has problems with perifery

--- truchet.py
+++ truchet.py
@@ -6,6 +6,10 @@
     Most entertaining.
     Basically there are two types of tiles - "/" and "\" and we just randomly
     generate the whole thing
+
+    What's extra here, is the flood fill http://en.wikipedia.org/wiki/Flood_fill
+    For that we first draw with cairo and then analyse on pixel level via
+    gtk.gdk.Image.
 """
 
 import gtk
@@ -59,6 +63,7 @@ class Canvas(graphics.Area):
         self.context.fill()
 
         self.inverse_fill()
+        #self.line_fill()  # line fill is much faster but has bugs in it
 
 
     def inverse_fill(self):
@@ -70,15 +75,14 @@ class Canvas(graphics.Area):
 
         colormap = image.get_colormap()
         color1 = colormap.alloc_color(255*260,255*240,255*240)
-        print color1.to_string()
 
         colors = [color1.pixel, 9684419]
         i = 1
         prev_row_color = colors[0]
 
+        # Trying to do checkers, going left-right, top-bottom
         for y in range(0, self.height - self.tile_size/2, self.tile_size):
-            if y > 0:
-                print "going for previous row"
+            if y > 0: # first row. TODO - this looks lame
                 prev_row_color = image.get_pixel(self.tile_size / 2, y - self.tile_size + self.tile_size / 2)
                 if prev_row_color not in colors:
                     prev_row_color = image.get_pixel(self.tile_size + self.tile_size / 2, y - self.tile_size + self.tile_size / 2)
@@ -92,8 +96,6 @@ class Canvas(graphics.Area):
             for x in range(0, self.width - self.tile_size / 2, self.tile_size):
                 current_color = image.get_pixel(x + self.tile_size / 2, y + self.tile_size / 2)
                 if current_color not in colors:
-                    print current_color
-
                     new_color = colors[1 - colors.index(prev_col_color)]
 
 
@@ -111,18 +113,68 @@ class Canvas(graphics.Area):
         self.window.draw_image(self.get_style().black_gc, image, 0, 0, 0, 0, -1, -1)
 
 
+    def line_fill(self):
+        """at the end i realized that we care only about lines"""
+        image = self.window.get_image(0, 0, self.width, self.height)
+        x, xd = 0, 1
+        y = 0
+
+        colormap = image.get_colormap()
+        color1 = colormap.alloc_color(255*260,255*240,255*240)
+        background = image.get_pixel(self.tile_size / 2, self.tile_size / 2)
+
+        colors = [color1.pixel, 9684419, background]
+        i = 0
+        paint_color = colors[0]
+        prev_match = -1
+
+
+        while y < self.height:
+            current_color = image.get_pixel(x, y)
+
+            current_match = -1
+            if current_color in colors:
+                current_match = colors.index(current_color)
+
+                if current_color == colors[2]:
+                    image.put_pixel(x, y, paint_color)
+            else:
+                if prev_match > -1:
+                    paint_color = colors[1 - colors.index(paint_color)] #variate between back and front
+
+            x += xd
+            if x >= self.width-1 or x < 0:
+                x = min(max(x, 0), self.width) #limit to bounds and turn around
+                xd = -xd
+                y+=1
+                if prev_match == -1:
+                    paint_color = colors[1 - colors.index(paint_color)] #variate between back and front
+
+            prev_match = current_match
+
+        self.window.draw_image(self.get_style().black_gc, image, 0, 0, 0, 0, -1, -1)
+
+
     def bucket_fill(self, image, x, y, old_color, new_color):
+        """whoa, a queue based 4-direction bucket fill
+           linescan could improve things
+        """
         queue = [(x,y)]
+
+        operations = 0
         while queue:
             x, y = queue.pop(0)
+            if image.get_pixel(x, y) == new_color:
+                operations += 1
 
-            if abs(image.get_pixel(x, y) - old_color) < 4000000:
+            current_color = image.get_pixel(x, y)
+            if current_color!= new_color and abs(current_color - old_color) < 4000000:
                 image.put_pixel(x, y, new_color)
-                if x > 0: queue.append((x - 1, y))
-                if x < self.width: queue.append((x + 1, y))
-                if y > 0: queue.append((x, y - 1))
-                if y < self.height: queue.append((x, y + 1))
-
+                if x > 0 and image.get_pixel(x-1, y) != new_color: queue.append((x - 1, y))
+                if x < self.width and image.get_pixel(x+1, y) != new_color: queue.append((x + 1, y))
+                if y > 0 and image.get_pixel(x, y-1) != new_color: queue.append((x, y - 1))
+                if y < self.height and image.get_pixel(x, y+1) != new_color: queue.append((x, y + 1))
+        print operations, "operations on fill"
 
 
 class BasicWindow:
