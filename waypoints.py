6ec4da62ea5ceed8bd00cb9c4c15f9c2640988f6
Toms Bauģis <toms.baugis@gmail.com>
2010-02-04 23:36:14 +0000

    added some useless waypoints.

--- waypoints.py
+++ waypoints.py
@@ -13,7 +13,7 @@ from lib import graphics, pytweener
 from lib.pytweener import Easing
 
 import math
-from random import random
+import random
 
 from lib.euclid import Vector2, Point2
 from lib.proximity import LQProximityStore
@@ -37,9 +37,16 @@ class Waypoint(object):
         else:
             boid.target(self.next)
 
+        boid.velocity *= 4
+
     def update(self):
         pass
 
+    def get_next(self, boid):
+        if boid.data and "reverse" in boid.data:
+            return self.previous
+        else:
+            return self.next
 
 class QueueingWaypoint(Waypoint):
     """waypoint that eats boids and then releases them after a set period"""
@@ -75,7 +82,6 @@ class QueueingWaypoint(Waypoint):
 
 
 
-
 class BucketWaypoint(Waypoint):
     """waypoint that will queue our friends until required number
        arrives and then let them go"""
@@ -84,6 +90,7 @@ class BucketWaypoint(Waypoint):
         self.bucket_size = bucket_size
         self.boids = []
         self.rotation_angle = 0
+        self.radius = 40
 
 
     def see_you(self, boid):
@@ -92,22 +99,80 @@ class BucketWaypoint(Waypoint):
         if boid not in self.boids and (self.location - boid.location).magnitude_squared() < 400:
             self.boids.append(boid)
         else: #start braking
-            boid.velocity *= 0.9
+            boid.velocity *= 0.95
 
+    def update(self):
         if len(self.boids) == self.bucket_size:
             for boid in self.boids:
                 self.move_on(boid)
 
             self.boids = []
 
+
         self.rotation_angle += 0.02
         angle_step = math.pi * 2 / (self.bucket_size - 1)
         current_angle = 0
         for boid in self.boids:
             current_angle += angle_step
-            boid.location.x = self.location.x + math.cos(self.rotation_angle + current_angle) * 20
-            boid.location.y = self.location.y + math.sin(self.rotation_angle + current_angle) * 20
+            boid.location.x = self.location.x + math.cos(self.rotation_angle + current_angle) * self.radius
+            boid.location.y = self.location.y + math.sin(self.rotation_angle + current_angle) * self.radius
             boid.velocity = (self.location - boid.location).cross() * 0.01
+
+
+class CarouselWaypoint(Waypoint):
+    """waypoint that will queue our friends until required number
+       arrives and then let them go"""
+    def __init__(self, x, y, radius):
+        Waypoint.__init__(self, x, y)
+        self.boids = []
+        self.rotation_angle = 0
+        self.radius = radius
+
+        self.boid_angles = {}
+        self.boid_revolutions = {}
+
+        self.speed = 1
+        self.revolutions = 0.1
+
+
+    def see_you(self, boid):
+        if boid not in self.boids:
+            if (self.location - boid.location).magnitude_squared() < self.radius * self.radius:
+                if not self.boids:
+                    self.rotation_angle = (boid.location - self.location).heading()
+                self.boids.append(boid)
+                self.boid_angles[boid] = (boid.location - self.location).heading()
+                self.boid_revolutions[boid] = 0
+
+
+    def update(self):
+        poplist = []
+        for boid in self.boids:
+            if self.boid_revolutions[boid] > self.revolutions:
+                angle = self.boid_angles[boid] % (math.pi * 2)
+                if math.pi -0.1 < abs((self.location - self.get_next(boid).location).heading() - angle) < math.pi:
+                    self.move_on(boid)
+                    poplist.append(boid)
+
+        for boid in poplist:
+            self.boids.remove(boid)
+
+
+        if self.boids:
+            step = self.speed / 50.0
+            for boid in self.boids:
+                self.boid_angles[boid] += step
+                self.boid_revolutions[boid] += step / math.pi / 2
+                boid.location.x = self.location.x + math.cos(self.boid_angles[boid]) * self.radius
+                boid.location.y = self.location.y + math.sin(self.boid_angles[boid]) * self.radius
+                boid.velocity = (boid.location - self.location) * 0.01
+
+
+
+        self.rotation_angle += 0.05 #* math.sqrt(len(self.boids) / float(self.bucket_size))
+        self.incoming = 0
+
+
 
 
 class GrowWaypoint(Waypoint):
@@ -132,35 +197,48 @@ class GrowWaypoint(Waypoint):
 
 
 
+class ShakyWaypoint(Waypoint):
+    def __init__(self, x, y):
+        Waypoint.__init__(self, x, y)
 
+    @staticmethod
+    def virus(boid, data):
+        seizure = random.random() > 0.4
+        if seizure:
+            boid.radius = data.setdefault('radius', boid.radius) * (random.random() * 4)
+
+    def see_you(self, boid):
+
+        if boid.virus:
+            boid.virus = None
+            boid.radius = boid.data['radius']
+        else:
+            boid.virus = self.virus
+        self.move_on(boid)
 
 
 class Boid(object):
-    # distances are squared to avoid roots (slower)
-    separation = 15.0 # separation from other objects
-    awareness = 30.0  # the sense field
-    separation_squared = separation * separation
-    awareness_squared = awareness * awareness
-
-
-    def __init__(self, location):
+    def __init__(self, location, max_speed = 2.0):
         self.visible = True
         self.radius = 3
         self.acceleration = Vector2()
         self.brake = Vector2()
-        self.velocity = Vector2(random() * 2 - 1, random() * 2 - 1)
+        self.velocity = Vector2(random.random() * 2 - 1, random.random() * 2 - 1)
         self.location = location
-        self.max_speed = 2.0
+        self.max_speed = max_speed
         self.max_force = 0.03
         self.positions = []
         self.message = None # a message that waypoint has set perhaps
-        self.data = None
 
-        self.radio = self.awareness
+        self.data = {}
+        self.virus = None
+
+        self.radio = self.radius * 5
 
         self.target_waypoint = None
 
     def target(self, waypoint):
+        self.radio = self.radius * 5
         self.target_waypoint = waypoint
 
     def run(self, flock_boids):
@@ -176,7 +254,6 @@ class Boid(object):
 
         if (self.location - self.target_waypoint.location).magnitude_squared() < self.radio * self.radio:
             self.target_waypoint.see_you(self) #tell waypoint that we see him and hope that he will direct us further
-            self.radio = self.awareness
 
         self.radio += 0.3
 
@@ -184,6 +261,10 @@ class Boid(object):
         self.location += self.velocity
 
         self.acceleration *= 0
+
+        if self.virus:
+            self.virus(self, self.data)
+
         #if not self.positions or int(self.location.x) != int(self.positions[-1].x) and int(self.location.y) != int(self.positions[-1].y):
         #    self.positions.append(Vector2(self.location.x, self.location.y))
 
@@ -224,7 +305,7 @@ class Boid(object):
             if not boid.visible:
                 continue
 
-            if 0 < d < self.separation_squared:
+            if 0 < d < self.radius * 5 * self.radius * 5:
                 diff = self.location - boid.location
                 diff.normalize()
                 diff = diff / math.sqrt(d)  # Weight by distance
@@ -253,8 +334,8 @@ class Boid(object):
 
 
             # Two options for desired vector magnitude (1 -- based on distance, 2 -- maxspeed)
-            if  slow_down and d < self.awareness_squared:
-                desired *= self.max_speed * (math.sqrt(d) / self.awareness) # This damping is somewhat arbitrary
+            if  slow_down and d < self.radius * 5 * self.radius * 5:
+                desired *= self.max_speed * (math.sqrt(d) / self.radius * 5) # This damping is somewhat arbitrary
             else:
                 desired *= self.max_speed
 
@@ -267,46 +348,6 @@ class Boid(object):
 
 
 class Canvas(graphics.Area):
-    def __init__(self):
-        graphics.Area.__init__(self)
-
-        self.connect("mouse-move", self.on_mouse_move)
-        self.connect("button-press", self.on_mouse_button_press)
-        self.connect("mouse-click", self.on_mouse_click)
-
-        # we should redo the boxes when window gets resized
-        box_size = 10
-        self.proximities = LQProximityStore(Vector2(0,0), Vector2(600,400), box_size)
-
-        self.waypoints = []
-        self.waypoints = [QueueingWaypoint(100, 100, 70),
-                          BucketWaypoint(500, 100, 5),
-                          GrowWaypoint(300, 200, 8),
-                          GrowWaypoint(100, 500, 3),
-                          Waypoint(500, 500)]
-
-        # link them together
-        for curr, next in zip(self.waypoints, self.waypoints[1:]):
-            curr.next = next
-            next.previous = curr
-
-        self.waypoints[0].previous = self.waypoints[-1]
-        self.waypoints[-1].next = self.waypoints[0]
-
-
-
-        self.boids = [Boid(Vector2(120,10)),
-                      Boid(Vector2(150,10)),
-                      Boid(Vector2(160,200)),
-                      Boid(Vector2(350,100)),
-                      Boid(Vector2(150,10))
-                      ]
-        for i, boid in enumerate(self.boids):
-            boid.target(self.waypoints[0])
-
-        self.mouse_node = None
-
-
     # just for kicks - mouse events
     def on_mouse_button_press(self, area, over_regions):
         if over_regions:
@@ -336,32 +377,35 @@ class Canvas(graphics.Area):
         for waypoint in self.waypoints:
             waypoint.update()
 
+
+        for boid in self.boids:
+            # the growing antennae circle
+            if self.debug_radius:
+                self.set_color("#ddd", 0.3)
+                self.context.arc(boid.location.x,
+                                 boid.location.y,
+                                 boid.radio,
+                                 -math.pi, math.pi)
+                self.context.fill()
+
+
+            # obstacle awareness circle
+            if self.debug_awareness:
+                self.set_color("#ddd", 0.5)
+                self.context.arc(boid.location.x,
+                                 boid.location.y,
+                                 boid.awareness,
+                                 -math.pi, math.pi)
+                self.context.fill()
+
+
+
         for boid in self.boids:
             neighbours = self.proximities.find_neighbours(boid, 40)
 
             boid.run(neighbours)
 
             self.proximities.update_position(boid)
-
-            # the growing antennae circle
-            """
-            self.set_color("#ddd", 0.3)
-            self.context.arc(boid.location.x,
-                             boid.location.y,
-                             boid.radio,
-                             -math.pi, math.pi)
-            self.context.fill()
-            """
-
-            # obstacle awareness circle
-            """
-            self.set_color("#aaa", 0.5)
-            self.context.arc(boid.location.x,
-                             boid.location.y,
-                             boid.awareness,
-                             -math.pi, math.pi)
-            self.context.fill()
-            """
 
 
             # debug trail (if enabled)
@@ -373,7 +417,7 @@ class Canvas(graphics.Area):
 
 
             # sir boid himself
-            self.set_color("#666")
+            self.set_color("#888")
             boid.draw(self.context)
 
             # line between boid and it's target
@@ -383,6 +427,7 @@ class Canvas(graphics.Area):
                                  boid.target_waypoint.location.y)
             """
 
+            self.context.fill_preserve()
             self.context.stroke()
 
 
@@ -394,6 +439,47 @@ class Canvas(graphics.Area):
             self.context.fill()
 
         self.redraw_canvas()
+
+
+    def __init__(self):
+        graphics.Area.__init__(self)
+
+        self.connect("mouse-move", self.on_mouse_move)
+        self.connect("button-press", self.on_mouse_button_press)
+        self.connect("mouse-click", self.on_mouse_click)
+
+        # we should redo the boxes when window gets resized
+        box_size = 10
+        self.proximities = LQProximityStore(Vector2(0,0), Vector2(600,400), box_size)
+
+        self.waypoints = []
+        self.waypoints = [QueueingWaypoint(100, 100, 40),
+                          ShakyWaypoint(400, 200),
+                          CarouselWaypoint(500, 500, 30),
+                          GrowWaypoint(200, 400, 8),
+                          GrowWaypoint(200, 300, 3),
+                          ]
+
+        # link them together
+        for curr, next in zip(self.waypoints, self.waypoints[1:]):
+            curr.next = next
+            next.previous = curr
+
+        self.waypoints[0].previous = self.waypoints[-1]
+        self.waypoints[-1].next = self.waypoints[0]
+
+
+
+        self.boids = [Boid(Vector2(100,100), 2.0) for i in range(10)]
+
+        for i, boid in enumerate(self.boids):
+            boid.target(self.waypoints[0])
+
+        self.mouse_node = None
+
+        # some debug variables
+        self.debug_radius = False
+        self.debug_awareness = False
 
 
 
