d7aa04a8f24f848207debefcdee3c0e0cc8c2e86
Toms Bauģis <toms.baugis@gmail.com>
2010-02-17 19:24:02 +0000

    errm internal tangent arcs

---
+++ tangent_arcs.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# - coding: utf-8 -
+# Copyright (C) 2010 Toms Bauģis <toms.baugis at gmail.com>
+""" guilloches, following.  observe how detail grows and your cpu melts.
+    move mouse horizontally and vertically to change parameters
+    http://ministryoftype.co.uk/words/article/guilloches/
+"""
+
+import gtk
+from lib import graphics
+from lib.euclid import Vector2
+
+import math
+
+
+class Canvas(graphics.Scene):
+    def __init__(self):
+        graphics.Scene.__init__(self)
+
+        self.circle1 = graphics.Circle(90, x=100, y=300, pivot_x = 90, pivot_y = 90, interactive = True, draggable=True, stroke = "#000", fill="#ccc")
+        self.circle2 = graphics.Circle(50, x=350, y=300, pivot_x = 50, pivot_y = 50, interactive = True, draggable=True, stroke = "#000", fill="#ccc")
+
+        self.add_child(self.circle1)
+        self.add_child(self.circle2)
+
+        self.tangent = graphics.Sprite(interactive = False)
+        self.add_child(self.tangent)
+
+        self.draw_tangent()
+
+        self.connect("on-drag", self.on_drag_circle)
+
+    def on_drag_circle(self, scene, event, targets):
+        self.draw_tangent()
+
+    def draw_tangent(self):
+        tangent = self.tangent
+        tangent.graphics.clear()
+
+        band_radius = 10
+
+        v1 = Vector2(self.circle1.x, self.circle1.y)
+        v2 = Vector2(self.circle2.x, self.circle2.y)
+
+        distance = abs(v1 - v2)
+
+
+
+        tangent.graphics.set_color("#000")
+        #tangent.graphics.move_to(v1.x, v1.y)
+        #tangent.graphics.line_to(v2.x, v2.y)
+
+        c = distance
+        distance = max(distance*3, self.circle1.radius, self.circle2.radius) + 1
+
+        a = distance + self.circle2.radius
+        b = distance + self.circle1.radius
+
+
+        orientation = (v2-v1).heading()
+
+        # errrm, well, basically the one is in the other
+        if (b**2 + c**2 - a**2) / (2.0 * b * c) >= 1:
+            tangent.graphics.arc(v1.x, v1.y, max(self.circle1.radius, self.circle2.radius) + band_radius, 0, math.pi * 2)
+            tangent.graphics.stroke()
+            return
+
+
+        # we have to figure out the angle for the vector that is pointing
+        # towards the point C (which will help as to draw that tangent)
+        left_angle = math.acos((b**2 + c**2 - a**2) / (2.0 * b * c))
+        arc_angle = math.acos((a**2 + b**2 - c**2) / (2.0 * a * b))
+
+        # arc on the one side
+        a1 = left_angle + orientation
+        x, y = math.cos(a1) * b, math.sin(a1) * b
+
+        v3_1 = Vector2(v1.x+x, v1.y+y)
+        tangent.graphics.arc(v3_1.x, v3_1.y, distance - band_radius, (v1 - v3_1).heading(), (v2 - v3_1).heading())
+        tangent.graphics.stroke()
+
+
+
+        # arc on the other side (could as well flip at the orientation axis, too dumb to do that though)
+        a2 = -left_angle + orientation
+        x, y = math.cos(a2) * b, math.sin(a2) * b
+        v3_2 = Vector2(v1.x+x, v1.y+y)
+
+        tangent.graphics.arc(v3_2.x, v3_2.y, distance - band_radius, (v2 - v3_2).heading(), (v1 - v3_2).heading())
+        tangent.graphics.stroke()
+
+
+
+        # the rest of the circle
+        tangent.graphics.arc(v1.x, v1.y, self.circle1.radius + band_radius, (v3_1-v1).heading(), (v3_2-v1).heading())
+        tangent.graphics.stroke()
+
+        tangent.graphics.arc_negative(v2.x, v2.y, self.circle2.radius + band_radius, (v3_1-v2).heading(), (v3_2-v2).heading())
+
+        tangent.graphics.stroke()
+
+
+class BasicWindow:
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_size_request(800, 600)
+        window.connect("delete_event", lambda *args: gtk.main_quit())
+
+        canvas = Canvas()
+
+        box = gtk.VBox()
+        box.pack_start(canvas)
+
+
+        window.add(box)
+        window.show_all()
+
+
+if __name__ == "__main__":
+    example = BasicWindow()
+    gtk.main()
