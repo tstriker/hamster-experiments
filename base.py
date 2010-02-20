a94a460bdfeaa42c7655e22bb5b51e83c1c0a8ad
Toms Bauģis <toms.baugis@gmail.com>
2010-02-20 22:31:12 +0000

    plain handy so i don't copy bad code all over the place

---
+++ base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# - coding: utf-8 -
+# Copyright (C) 2010 Toms Bauģis <toms.baugis at gmail.com>
+
+"""Base template"""
+
+
+import gtk
+from lib import graphics
+
+class Scene(graphics.Scene):
+    def __init__(self):
+        graphics.Scene.__init__(self)
+
+class BasicWindow:
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_size_request(600, 500)
+        window.connect("delete_event", lambda *args: gtk.main_quit())
+        window.add(Scene())
+        window.show_all()
+
+example = BasicWindow()
+gtk.main()
