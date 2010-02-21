5889f8c60dfee45d3e60c15f6c3070a058387c02
Toms Bauģis <toms.baugis@gmail.com>
2010-02-21 19:20:42 +0000

    the blue ones seem to be doing best

--- random_movement.py
+++ random_movement.py
@@ -12,7 +12,7 @@ import gtk
 from lib import graphics
 from lib.euclid import Vector2
 import math
-from random import random
+from random import random, randint
 
 class Boid(graphics.Shape):
     def __init__(self):
@@ -60,18 +60,74 @@ class Boid1(Boid):
         self.acceleration += (Vector2(w/2, h/2) - self.location) / 4000
 
         self.velocity += self.acceleration
+        self.velocity.limit(2)
+        self.location += self.velocity
+
+
+class Boid2(Boid):
+    """acceleration is in slight angular deviation of velocity
+       maintaining in screen with the gravitational pull
+    """
+    def __init__(self):
+        Boid.__init__(self)
+        self.stroke_color = "#0f0"
+
+    def update_position(self, w, h):
+        acc_angle = self.velocity.heading() + (random() * 2 - 1)
+
+        max_distance = w/2 * w/2 + h/2 * h/2
+        current_centre_distance = (Vector2(w/2, h/2) - self.location).magnitude_squared()
+
+        self.acceleration = Vector2(math.cos(acc_angle), math.sin(acc_angle)) * 0.8
+        self.acceleration += (Vector2(w/2, h/2) - self.location) / 500 * (1 - (current_centre_distance / float(max_distance)))
+
+        self.velocity += self.acceleration
         self.velocity.limit(5)
         self.location += self.velocity
-        self.location.x = max(min(self.location.x, w), 0)
-        self.location.y = max(min(self.location.y, h), 0)
 
+        self.location.x = max(min(self.location.x, w + 100), -100)
+        self.location.y = max(min(self.location.y, h + 100), -100)
+
+
+class Boid3(Boid):
+    """waypoint oriented - once reached, picks another random waypoint
+       alternatively another random waypoint is picked, if the boid keeps
+       moving away from the current one - this way we can keep the speed constant
+       and discard waypoint instead
+    """
+    def __init__(self):
+        Boid.__init__(self)
+        self.stroke_color = "#00f"
+        self.target = None
+        self.prev_distance = None
+
+    def update_position(self, w, h):
+        distance = 0
+        if self.target:
+            distance = (self.target - self.location).magnitude_squared()
+
+        if not self.target or self.prev_distance and distance > self.prev_distance:
+            self.prev_distance = w * w + h * h
+            self.target = Vector2(randint(0, w), randint(0, h))
+            print self.target
+
+        target = (self.target - self.location)
+
+        self.acceleration = (target - self.velocity).normalize() * 0.25
+        self.velocity += self.acceleration
+
+        self.velocity.limit(6)
+
+        self.prev_distance = distance
+
+        self.location += self.velocity
 
 
 class Scene(graphics.Scene):
     def __init__(self):
         graphics.Scene.__init__(self)
 
-        self.boids = [Boid1()]
+        self.boids = [Boid1(), Boid2(), Boid3(), Boid3(), Boid3()]
         self.add_child(*self.boids)
         self.connect("on-enter-frame", self.on_enter_frame)
 
