f1c3e46a52b6424d83fc9ef748dbdb5dd4fe8aef
Toms Bauģis <toms.baugis@gmail.com>
2010-03-03 12:56:01 +0000

    no need for indexes yet - the documentation is too small

--- docs/conf.py
+++ docs/conf.py
@@ -140,10 +140,10 @@ html_theme = 'default'
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_use_modindex = True
+html_use_modindex = False
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
