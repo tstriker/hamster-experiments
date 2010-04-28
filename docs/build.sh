6b15ede5651c3a6594dff6e61c7370cfd68fedd0
Toms Bauģis <toms.baugis@gmail.com>
2010-04-28 12:29:08 +0100

    stashing before generating docs and popping stash after. allows to generate docs in a dirty state

--- docs/build.sh
+++ docs/build.sh
@@ -2,10 +2,11 @@
 rm -Rf _build/
 make html
 python sphinxtogithub.py _build/html/
+git stash
 git checkout gh-pages
 cp -R _build/html/* ../
 git add ../*.html ../*.js ../objects.inv ../static/* ../sources
 git commit -m "automatic documentation sync"
 git push
 git checkout master
-
+git stash pop
