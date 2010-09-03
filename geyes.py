45611f3e490cdccf02df8b849dc0bb0952d50ef6
Toms Bauģis <toms.baugis@gmail.com>
2010-09-03 20:58:45 +0100

    omg

---
+++ geyes.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# - coding: utf-8 -
+# Copyright (C) 2010 Toms Bauģis <toms.baugis at gmail.com>
+
+"""Guess what, haha"""
+
+
+import gtk
+from lib import graphics
+import math
+
+class Eye(graphics.Sprite):
+    def __init__(self, x, y, width, height):
+        graphics.Sprite.__init__(self, x = x, y = y)
+        self.angle = 0
+        self.pupil_distance = 0
+        self.width = width
+        self.height = height
+        self.connect("on-render", self.on_render)
+
+    def update(self, mouse_x, mouse_y):
+        width, height = (mouse_x - self.x), (mouse_y - self.y)
+
+        self.pupil_rotation = math.atan2(height, width)
+        self.pointer_distance = math.sqrt(width**2 + height**2)
+
+
+
+    def on_render(self, sprite):
+        width, height = self.width, self.height
+        self.graphics.ellipse(-width / 2, -height / 2, width, height)
+        self.graphics.fill("#fff")
+
+        rotation = self.pupil_rotation
+
+        pupil_x = min(math.cos(rotation) * width * self.pointer_distance / 1000, width * 0.2)
+        pupil_y = min(math.sin(rotation) * height * self.pointer_distance / 1000, height * 0.2)
+
+        self.graphics.circle(pupil_x, pupil_y, min(width / 4.0, height / 4.0))
+        self.graphics.fill("#000")
+
+
+
+class Scene(graphics.Scene):
+    def __init__(self):
+        graphics.Scene.__init__(self, framerate = 20)
+        self.eyes = [Eye(50, 100, 70, 100),
+                     Eye(150, 100, 70, 100)]
+        self.add_child(*self.eyes)
+        self.connect("on-enter-frame", self.on_enter_frame)
+
+    def on_enter_frame(self, scene, context):
+        for eye in self.eyes:
+            eye.update(self.mouse_x, self.mouse_y)
+
+        self.redraw()
+
+class BasicWindow:
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_size_request(200, 200)
+        window.connect("delete_event", lambda *args: gtk.main_quit())
+        window.add(Scene())
+        window.show_all()
+
+example = BasicWindow()
+gtk.main()
