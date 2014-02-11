28dcdf5b1bb9f8ba6ab1d975721647617a8ff20e
Toms Bauģis <toms.baugis@gmail.com>
2014-02-10 21:34:18 -0500

    improve boilerplate

--- base.py
+++ base.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # - coding: utf-8 -
-# Copyright (C) 2014 You <your.email@someplace>
+# Copyright (C) 2014 Toms Baugis <toms.baugis@gmail.com>
 
 """Base template"""
-
 
 from gi.repository import Gtk as gtk
 from lib import graphics
@@ -11,6 +10,14 @@ from lib import graphics
 class Scene(graphics.Scene):
     def __init__(self):
         graphics.Scene.__init__(self)
+
+        self.connect("on-enter-frame", self.on_enter_frame)
+
+    def on_enter_frame(self, scene, context):
+        # you could do all your drawing here, or you could add some sprites
+        g = graphics.Graphics(context)
+
+        # self.redraw() # this is how to get a constant redraw loop (say, for animation)
 
 
 
@@ -21,6 +28,7 @@ class BasicWindow:
         window.connect("delete_event", lambda *args: gtk.main_quit())
         window.add(Scene())
         window.show_all()
+
 
 if __name__ == '__main__':
     window = BasicWindow()
