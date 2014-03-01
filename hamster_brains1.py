2660629a90d7f4bfc9c154f31e20fd0ea1705d0d
Toms Bauģis <toms.baugis@gmail.com>
2014-03-01 18:36:07 -0500

    trying to refactor stats into something more reusable

--- hamster_brains1.py
+++ hamster_brains1.py
@@ -16,16 +16,105 @@ from lib import layout
 
 from hamster.client import Storage
 
-def minutes(delta):
-    return delta.total_seconds() / 60.0
+def minutes(facts):
+    time = dt.timedelta()
+    for fact in facts:
+        time += fact.delta
+    return time.total_seconds() / 60.0
+
+
+class Stats(object):
+    def __init__(self, facts, toplevel_group=None):
+        self.groups = None # we run top-level groups on first call
+        self.toplevel_group = toplevel_group
+        self._facts = facts
+
+        self._range_start = None
+        self._range_end = None
+        self._update_groups()
+
+
+    @property
+    def facts(self):
+        return self._facts
+
+    @facts.setter
+    def set_facts(self, facts):
+        self._facts = facts
+        self._update_groups()
+
+    def _update_groups(self):
+        for fact in self.facts:
+            self._range_start = min(fact.date, self._range_start or fact.date)
+            self._range_end = max(fact.date, self._range_end or fact.date)
+
+        if not self.toplevel_group:
+            # in case when we have no grouping, we are looking for totals
+            self.groups = facts
+        else:
+            key_func = self.toplevel_group
+            self.groups = {key: list(facts) for key, facts in
+                               itertools.groupby(sorted(self.facts, key=key_func), key_func)}
+
+
+    def group(self, key_func):
+        # return the nested thing
+        res = {}
+        for key, facts in self.groups.iteritems():
+            res[key] = {nested_key: list(nested_facts) for nested_key, nested_facts in
+                         itertools.groupby(sorted(facts, key=key_func), key_func)}
+        return res
+
+
+
+    def by_week(self):
+        """return series by week, fills gaps"""
+        year_week = lambda date: (date.year, int(date.strftime("%W")))
+
+        weeks = []
+        start, end = self._range_start, self._range_end
+        for i in range(0, (end - start).days, 7):
+            weeks.append(year_week(start + dt.timedelta(days=i)))
+
+        # group and then fill gaps and turn into a list
+        res = self.group(lambda fact: year_week(fact.date))
+        for key, group in res.iteritems():
+            res[key] = [minutes(group.get(week, [])) for week in weeks]
+
+        return res
+
+
+    def by_weekday(self):
+        """return series by weekday, fills gaps"""
+        res = self.group(lambda fact: fact.date.weekday())
+        for key, group in res.iteritems():
+            res[key] = [minutes(group.get(weekday, [])) for weekday in range(7)]
+        return res
+
+
+    def by_hour(self):
+        """return series by hour, stretched for the duration"""
+
+
+    def sum_durations(self, keys):
+        """returns summed durations of the specified keys iterable"""
+        res = []
+        for key in keys:
+            res_delta = dt.timedelta()
+            for fact in (facts_dict.get(key) or []):
+                res_delta += fact.delta
+            res.append(round(res_delta.total_seconds() / 60.0))
+        return res
+
 
 class SparkBars(layout.Widget):
-    def __init__(self, items=None, width = None, height=None, color=None, **kwargs):
+    def __init__(self, items=None, width = None, height=None, color=None, gap=1, **kwargs):
         layout.Widget.__init__(self, **kwargs)
 
         self.width = width or 100
         self.height = height or 20
         self.bar_width = 10
+        self.gap = gap
         self.color = color or "#777"
         self.items = items or []
 
@@ -37,10 +126,8 @@ class SparkBars(layout.Widget):
         self.graphics.save_context()
         self.graphics.translate(0, self.height)
 
-        gap = 1
-
-        max_width = min(self.width, len(self.items) * (self.bar_width + gap))
-        pixels = graphics.full_pixels(max_width, [1] * len(self.items), gap)
+        max_width = min(self.width, len(self.items) * (self.bar_width + self.gap))
+        pixels = graphics.full_pixels(max_width, [1] * len(self.items), self.gap)
 
         max_val = max(self.items)
 
@@ -48,7 +135,7 @@ class SparkBars(layout.Widget):
             height = max(1, round(val * 1.0 / max_val * self.height))
 
             self.graphics.rectangle(0, 0, width, -height)
-            self.graphics.translate(width + gap, 0)
+            self.graphics.translate(width + self.gap, 0)
         self.graphics.fill(self.color)
 
         self.graphics.restore_context()
@@ -84,15 +171,20 @@ class Scene(graphics.Scene):
 
 
     def on_facts_loaded(self):
-        print "rendering"
-        by_activity = {}
-        for fact in self.facts:
-            activity = "%s@%s" % (fact.activity, fact.category)
-            rec = by_activity.setdefault(activity,
-                                         {"activity": fact.activity,
-                                          "category": fact.category,
-                                          "facts": []})
-            rec['facts'].append(fact)
+        stats = Stats(self.facts, lambda fact: (fact.category, fact.activity))
+        by_week = stats.by_week()
+
+        # group by weekday
+        by_weekday = stats.by_weekday()
+
+        # group by workday / holiday
+        by_work_hobby = stats.group(lambda fact: "weekend" if fact.date.weekday() in  (5, 6) else "workday")
+        for activity, group in by_work_hobby.iteritems():
+            work, non_work = group.get("workday", []), group.get("weekend", [])
+            total = minutes(work) + minutes(non_work)
+            by_work_hobby[activity] = "workday" if minutes(work) / total > 0.8 and len(work) > 10 else "other"
+
+
 
 
         self.clear()
@@ -113,41 +205,23 @@ class Scene(graphics.Scene):
         activity_weekdays[1].add_child(layout.Label("Weekdays", expand=False, x_align=0))
         activity_weekdays[2].add_child(layout.Label("By week", expand=False, x_align=0))
 
-        workdays = self.get_workday_pattern(by_activity)
 
-        activity_by_week = defaultdict(lambda: defaultdict(dt.timedelta))
-
-        start_date = None
-        now = dt.datetime.now()
-        year_week = lambda date: (date.year, int(date.strftime("%W")))
-        for activity, stats in by_activity.iteritems():
-            for fact in stats['facts']:
-                start_date = min(fact.date, start_date or fact.date)
-                activity_by_week[activity][year_week(fact.date)] += fact.delta
-
-        for activity, stats in by_activity.iteritems():
-            stats["by_year_week"] = []
-            for i in range(0, (now.date() - start_date).days, 7):
-                week = year_week(start_date + dt.timedelta(days=i))
-                stats["by_year_week"].append(activity_by_week[activity][week].total_seconds() / 60.0)
-
-
-        for activity in sorted([(rec["category"], rec["activity"]) for rec in by_activity.itervalues()]):
-            activity = "%s@%s" % (activity[1], activity[0])
-
-            label = layout.Label(activity, color="#333", size=12, x_align=0, y_align=0.5)
+        for activity in stats.groups.keys():
+            label = layout.Label("%s@%s" % (activity[1], activity[0]),
+                                 color="#333",
+                                 size=12, x_align=0, y_align=0.5)
             label.max_width = 150
             activity_weekdays[0].add_child(label)
 
-            if workdays[activity]["pattern"] == "workday":
+            if by_work_hobby[activity] == "workday":
                 color = graphics.Colors.category10[0]
             else:
                 color = graphics.Colors.category10[2]
 
-            hours = [workdays[activity]["by_weekday"].get(i, {}).get("total", 0) for i in range(7)]
+            hours = [rec for rec in by_weekday[activity]]
             activity_weekdays[1].add_child(SparkBars(hours, color=color))
 
-            weeks = by_activity[activity]["by_year_week"]
+            weeks = by_week[activity]
             activity_weekdays[2].add_child(SparkBars(weeks, width=200, color=color))
 
 
